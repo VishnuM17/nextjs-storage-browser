15c78ec52f75184bd73546392f4676263f13e744
root <root@ip-192-164-12-25.ec2.internal>
2024-11-08 13:43:02 +0000

    C3

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -21,18 +21,36 @@ import { defineStorage } from "@aws-amplify/backend";
 
 // // amplify/storage/resource.ts
 
+// export const storage = defineStorage({
+//   name: 'storage-browser-test',
+//   access: (allow: any) => ({
+//     'public/*': [
+//       allow.guest.to(['read', 'write']),
+//       allow.authenticated.to(['read', 'write', 'delete']),
+//     ],
+//     'protected/{entity_id}/*': [
+//       allow.authenticated.to(['read']),
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ],
+//     'private/{entity_id}/*': [
+//       allow.entity('identity').to(['read', 'write', 'delete'])
+//     ]
+//   })
+// });
+
 export const storage = defineStorage({
-  name: 'storage-browser-test',
+  name: "storage-browser-test",
   access: (allow: any) => ({
-    'public/*': [
-      allow.guest.to(['read', 'write']),
-      allow.authenticated.to(['read', 'write', 'delete']),
+    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+    'media-readonly/*': [allow.authenticated.to(['read'])],
+    'shared-folder-readwrite/*': [
+      allow.authenticated.to(['read', 'write'])
     ],
-    'protected/{entity_id}/*': [
+    'protected-useronlyreadwritedelete/{entity_id}/*': [
       allow.authenticated.to(['read']),
       allow.entity('identity').to(['read', 'write', 'delete'])
     ],
-    'private/{entity_id}/*': [
+    'private-useronlyreadwritedelete/{entity_id}/*': [
       allow.entity('identity').to(['read', 'write', 'delete'])
     ]
   })
--- app/page.tsx
+++ app/page.tsx
@@ -49,52 +49,105 @@
 // export default withAuthenticator(Example);
 
 
-"use client"
-import { Flex } from "@aws-amplify/ui-react";
-import { createAmplifyAuthAdapter, createStorageBrowser, elementsDefault } from "@aws-amplify/ui-react-storage/browser";
-import "@aws-amplify/ui-react-storage/styles.css";
+// "use client"
+// import { Flex } from "@aws-amplify/ui-react";
+// import { createAmplifyAuthAdapter, createStorageBrowser, elementsDefault } from "@aws-amplify/ui-react-storage/browser";
+// import "@aws-amplify/ui-react-storage/styles.css";
+// import { Amplify } from "aws-amplify";
+// import config from "../amplify_outputs.json";
+
+
+// Amplify.configure(config);
+// // these should match access patterns defined in amplify/storage/resource.ts
+// const input = {
+//   elements: elementsDefault,
+//   config: createAmplifyAuthAdapter({
+//     options: {
+//       defaultPrefixes: [
+//           'public/',
+//           (identityId: string) => `protected/${identityId}/`,
+//           (identityId: string) => `private/${identityId}/`,
+//         ]
+//     },
+//   }),
+// }
+// const { StorageBrowser, useControl } = createStorageBrowser(input);
+
+// function MyStorageBrowser() {
+//   const [{ selected }] = useControl('LOCATION_ACTIONS');
+
+//   return (
+//     <Flex>
+//       <Flex direction={'row'}>
+//         <StorageBrowser.LocationsView />
+//         <StorageBrowser.LocationDetailView />
+//       </Flex>
+//       {selected.type ? (
+//         <dialog open>
+//           <StorageBrowser.LocationActionView />
+//         </dialog>
+//       ) : null}
+//     </Flex>
+//   );
+// }
+
+// export default function CustomStorageBrowser() {
+//   return (
+//     <StorageBrowser.Provider>
+//       <MyStorageBrowser />
+//     </StorageBrowser.Provider>
+//   )
+// }
+
+
+'use client';
+import React from "react";
 import { Amplify } from "aws-amplify";
+import { signOut } from "aws-amplify/auth";
+
+import { Button, withAuthenticator } from "@aws-amplify/ui-react";
+import {
+  createStorageBrowser,
+  createAmplifyAuthAdapter,
+  elementsDefault,
+} from "@aws-amplify/ui-react-storage/browser";
+import "@aws-amplify/ui-react-storage/styles.css";
+import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
+
 import config from "../amplify_outputs.json";
 
-
 Amplify.configure(config);
-// these should match access patterns defined in amplify/storage/resource.ts
-const input = {
-  elements: elementsDefault,
-  config: createAmplifyAuthAdapter({
-    options: {
-      defaultPrefixes: [
-          'public/',
-          (identityId: string) => `protected/${identityId}/`,
-          (identityId: string) => `private/${identityId}/`,
-        ]
-    },
-  }),
-}
-const { StorageBrowser, useControl } = createStorageBrowser(input);
 
-function MyStorageBrowser() {
-  const [{ selected }] = useControl('LOCATION_ACTIONS');
+function Example() {
+  const { StorageBrowser } = createStorageBrowser({
+    elements: elementsDefault,
+    config: createAmplifyAuthAdapter({
+      options: {
+        defaultPrefixes: [
+          "media-readwritedelete/",
+          "media-readonly/",
+          "shared-folder-readwrite/",
+          (identityId: string) => `protected-useronlyreadwritedelete/${identityId}/`,
+          (identityId: string) => `private-useronlyreadwritedelete/${identityId}/`,
+        ],
+      },
+    }),
+  });
 
   return (
-    <Flex>
-      <Flex direction={'row'}>
-        <StorageBrowser.LocationsView />
-        <StorageBrowser.LocationDetailView />
-      </Flex>
-      {selected.type ? (
-        <dialog open>
-          <StorageBrowser.LocationActionView />
-        </dialog>
-      ) : null}
-    </Flex>
+    <>
+      <Button
+        marginBlockEnd="xl"
+        size="small"
+        onClick={() => {
+          signOut();
+        }}
+      >
+        Sign Out
+      </Button>
+      <StorageBrowser />
+    </>
   );
 }
 
-export default function CustomStorageBrowser() {
-  return (
-    <StorageBrowser.Provider>
-      <MyStorageBrowser />
-    </StorageBrowser.Provider>
-  )
-}
+export default withAuthenticator(Example);
