7e5c348329ccdc12b4ef33146e9c5b86d5a5e091
root <root@ip-192-164-12-25.ec2.internal>
2024-11-08 13:25:43 +0000

    C2

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,100 @@
-'use client';
-import React from "react";
-import { Amplify } from "aws-amplify";
-import { signOut } from "aws-amplify/auth";
+// 'use client';
+// import React from "react";
+// import { Amplify } from "aws-amplify";
+// import { signOut } from "aws-amplify/auth";
 
-import { Button, withAuthenticator } from "@aws-amplify/ui-react";
-import {
-  createStorageBrowser,
-  createAmplifyAuthAdapter,
-  elementsDefault,
-} from "@aws-amplify/ui-react-storage/browser";
+// import { Button, withAuthenticator } from "@aws-amplify/ui-react";
+// import {
+//   createStorageBrowser,
+//   createAmplifyAuthAdapter,
+//   elementsDefault,
+// } from "@aws-amplify/ui-react-storage/browser";
+// import "@aws-amplify/ui-react-storage/styles.css";
+// import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
+
+// import config from "../amplify_outputs.json";
+
+// Amplify.configure(config);
+
+// function Example() {
+//   const { StorageBrowser } = createStorageBrowser({
+//     elements: elementsDefault,
+//     config: createAmplifyAuthAdapter({
+//       options: {
+//         defaultPrefixes: [
+//          'public/',
+//           (identityId: string) => `protected/${identityId}/`,
+//           (identityId: string) => `private/${identityId}/`,
+//         ],
+//       },
+//     }),
+//   });
+
+//   return (
+//     <>
+//       <Button
+//         marginBlockEnd="xl"
+//         size="small"
+//         onClick={() => {
+//           signOut();
+//         }}
+//       >
+//         Sign Out
+//       </Button>
+//       <StorageBrowser />
+//     </>
+//   );
+// }
+
+// export default withAuthenticator(Example);
+
+
+"use client"
+import { Flex } from "@aws-amplify/ui-react";
+import { createAmplifyAuthAdapter, createStorageBrowser, elementsDefault } from "@aws-amplify/ui-react-storage/browser";
 import "@aws-amplify/ui-react-storage/styles.css";
-import "@aws-amplify/ui-react-storage/storage-browser-styles.css";
-
+import { Amplify } from "aws-amplify";
 import config from "../amplify_outputs.json";
 
-Amplify.configure(config);
 
-function Example() {
-  const { StorageBrowser } = createStorageBrowser({
-    elements: elementsDefault,
-    config: createAmplifyAuthAdapter({
-      options: {
-        defaultPrefixes: [
-         'public/',
+Amplify.configure(config);
+// these should match access patterns defined in amplify/storage/resource.ts
+const input = {
+  elements: elementsDefault,
+  config: createAmplifyAuthAdapter({
+    options: {
+      defaultPrefixes: [
+          'public/',
           (identityId: string) => `protected/${identityId}/`,
           (identityId: string) => `private/${identityId}/`,
-        ],
-      },
-    }),
-  });
+        ]
+    },
+  }),
+}
+const { StorageBrowser, useControl } = createStorageBrowser(input);
+
+function MyStorageBrowser() {
+  const [{ selected }] = useControl('LOCATION_ACTIONS');
 
   return (
-    <>
-      <Button
-        marginBlockEnd="xl"
-        size="small"
-        onClick={() => {
-          signOut();
-        }}
-      >
-        Sign Out
-      </Button>
-      <StorageBrowser />
-    </>
+    <Flex>
+      <Flex direction={'row'}>
+        <StorageBrowser.LocationsView />
+        <StorageBrowser.LocationDetailView />
+      </Flex>
+      {selected.type ? (
+        <dialog open>
+          <StorageBrowser.LocationActionView />
+        </dialog>
+      ) : null}
+    </Flex>
   );
 }
 
-export default withAuthenticator(Example);
+export default function CustomStorageBrowser() {
+  return (
+    <StorageBrowser.Provider>
+      <MyStorageBrowser />
+    </StorageBrowser.Provider>
+  )
+}
