51d6b67d773f9d0af70acaaed5b01fdd87716b68
root <root@ip-192-164-12-25.ec2.internal>
2024-11-11 05:59:19 +0000

    C4

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -21,37 +21,30 @@ import { defineStorage } from "@aws-amplify/backend";
 
 // // amplify/storage/resource.ts
 
+export const storage = defineStorage({
+  name: 'storage-browser-test',
+  access: (allow: any) => ({
+    'public/*': [
+      allow.guest.to(['read', 'write']),
+      allow.authenticated.to(['read', 'write', 'delete']),
+    ]
+  })
+});
+
 // export const storage = defineStorage({
-//   name: 'storage-browser-test',
+//   name: "storage-browser-test",
 //   access: (allow: any) => ({
-//     'public/*': [
-//       allow.guest.to(['read', 'write']),
-//       allow.authenticated.to(['read', 'write', 'delete']),
+//     'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
+//     'media-readonly/*': [allow.authenticated.to(['read'])],
+//     'shared-folder-readwrite/*': [
+//       allow.authenticated.to(['read', 'write'])
 //     ],
-//     'protected/{entity_id}/*': [
+//     'protected-useronlyreadwritedelete/{entity_id}/*': [
 //       allow.authenticated.to(['read']),
 //       allow.entity('identity').to(['read', 'write', 'delete'])
 //     ],
-//     'private/{entity_id}/*': [
+//     'private-useronlyreadwritedelete/{entity_id}/*': [
 //       allow.entity('identity').to(['read', 'write', 'delete'])
 //     ]
 //   })
 // });
-
-export const storage = defineStorage({
-  name: "storage-browser-test",
-  access: (allow: any) => ({
-    'media-readwritedelete/*': [allow.authenticated.to(['read', 'write', 'delete'])],
-    'media-readonly/*': [allow.authenticated.to(['read'])],
-    'shared-folder-readwrite/*': [
-      allow.authenticated.to(['read', 'write'])
-    ],
-    'protected-useronlyreadwritedelete/{entity_id}/*': [
-      allow.authenticated.to(['read']),
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ],
-    'private-useronlyreadwritedelete/{entity_id}/*': [
-      allow.entity('identity').to(['read', 'write', 'delete'])
-    ]
-  })
-});
